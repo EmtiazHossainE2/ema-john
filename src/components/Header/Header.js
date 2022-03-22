8a6a225b0701ba0d817286d79bf4511b93fcd010
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-22 15:54:08 +0600

    responsive added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
+import menu from '../../images/menu.png'
+
 
 const Header = () => {
+
+
     return (
         <div>
             <nav className="container">
                 <a href="./home"><img src={logo} alt="" /></a>
                 <div>
-                    <ul>
+                    <ul id="MenuItems">
                         <li><a href="./shop">Shop</a></li>
                         <li><a href="./order">Order</a></li>
                         <li><a href="./review">Order Review</a></li>
                         <li><a href="./inventory">Manage Inventory</a></li>
                     </ul>
+                    <img src={menu} alt="" class="menu-icon" ></img>
                 </div>
             </nav>
         </div>
