f9b9cc4b361bb30959850a248a2f7c7c4dafb014
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-23 21:36:45 +0600

    get exit data

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -8,8 +8,11 @@ const Cart = (props) => {
 
     let total = 0
     let shipping = 0
+    let quantity = 0
     for (const product of cart) {
-        total = total + product.price
+        quantity = quantity + product.quantity
+        // total = total + product.price
+        total = total + product.price * product.quantity
         shipping = shipping + product.shipping
     }
 
@@ -19,7 +22,8 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items : {cart.length}</p>
+            {/* <p>Selected Items : {cart.length}</p> */}
+            <p>Selected Items : {quantity}</p>
             <p>Total Price : ${total} </p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax : ${tax} </p>
