f201a3d1f2dc8a3d09c81bf21154e8a89cda6935
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-03-31 16:41:35 +0600

    order summary get frop shop another route

---
+++ src/components/OrderSummary/OrderSummary.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderSummary = () => {
+    return (
+        <div>
+            <h2>Order Summary</h2>
+        </div>
+    );
+};
+
+export default OrderSummary;
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import useCart from '../../hooks/useCart';
 import useProducts from '../../hooks/useProducts';
+import Cart from '../Cart/Cart';
+import OrderItems from '../OrderItems/OrderItems';
+import OrderSummary from '../OrderSummary/OrderSummary';
 
 const Orders = () => {
     const [products, setProducts] = useProducts()
     const [cart, setCart] = useCart(products)
     return (
         <div>
-            <h1>Order Page</h1>
-            <h2>Total product {products.length}</h2>
-            <h2>total order : {cart.length}</h2>
+            <h1 className='text-center my-3'>Order Page</h1>
+            <div className="container">
+                <div className="row  mt-5">
+                    <div className="col-lg-6 text-center">
+                        <OrderSummary></OrderSummary>
+                    </div>
+                    <div className="col-lg-6">
+                        <Cart cart={cart}></Cart>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
