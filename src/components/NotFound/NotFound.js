6c0f42fac6b862ee8197f0bab2d0a672799d4e2b
EmtiazHossainE2 <emtiazemon310@gmail.com>
2022-04-10 12:59:22 +0600

    file transfer

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const NotFound = () => {
     return (
         <div className='text-center mt-5'>
-            <h2>This page is  Not AvailAble</h2>
+            <h2>This page is  Not AvailAble .tata bye bye </h2>
             <h1 className='mb-4'>404 </h1>
             <Link to='/'><button className='btn btn-warning'>Back Home</button></Link>
 
